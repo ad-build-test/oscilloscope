5ae51a1d3ecf89197120c132bbfe35bfd1390e19
Garth Brown <gwbrown@slac.stanford.edu>
2019-05-17 15:36:00 -0700

    Fixing IOC name

---
+++ iocBoot/sioc-lgun-sc01/st.cmd
@@ -0,0 +1,21 @@
+#!../../bin/rhel6-x86_64/oscilloscope
+
+#- You may have to change oscilloscope to something else
+#- everywhere it appears in this file
+
+< envPaths
+
+epicsEnvSet("IOC_NAME", "SIOC:LGUN:LS02")
+epicsEnvSet("NODE_NAME", "scop-lgun-ls02")
+epicsEnvSet("P", "SCOP:LGUN:LS02")
+
+< ../common/generalInit.cmd
+pwd()
+< iocBoot/common/tds.cmd
+
+iocInit
+
+## Start any sequence programs
+#seq sncxxx,"user=gwbrown"
+
+< ../common/restore.cmd
