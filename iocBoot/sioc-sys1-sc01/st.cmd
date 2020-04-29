45690154d6e1c34a85d38e1ba75de056f249800c
gwbrown <gwbrown@slac.stanford.edu>
2020-04-29 15:03:41 -0700

    Staging support for all general purpose FACET scopes. They still have working support from older software, so don't break it yet.

---
+++ iocBoot/sioc-sys1-sc01/st.cmd
@@ -0,0 +1,34 @@
+#!../../bin/rhel6-x86_64/oscilloscope
+
+#- Support for general purpose oscilloscopes
+
+< envPaths
+
+epicsEnvSet("IOC_NAME", "SIOC:SYS0:SC01")
+
+< ../common/generalInit.cmd
+pwd()
+
+epicsEnvSet("NODE_NAME", "scop-la20-ls21")
+epicsEnvSet("P", "SCOP:LA20:LS21")
+< iocBoot/common/tds.cmd
+
+epicsEnvSet("NODE_NAME", "scop-li20-ex01")
+epicsEnvSet("P", "SCOP:LI20:EX01")
+< iocBoot/common/tds.cmd
+
+epicsEnvSet("NODE_NAME", "scop-li20-ex02")
+epicsEnvSet("P", "SCOP:LI20:EX02")
+< iocBoot/common/tds.cmd
+
+epicsEnvSet("NODE_NAME", "scop-li20-los1")
+epicsEnvSet("P", "SCOP:LI20:LOS1")
+< iocBoot/common/tds.cmd
+
+epicsEnvSet("NODE_NAME", "scop-in10-fc01")
+epicsEnvSet("P", "SCOP:MCC0:SC02")
+< iocBoot/common/tds.cmd
+
+iocInit
+
+< iocBoot/common/autoSaveRun.cmd
