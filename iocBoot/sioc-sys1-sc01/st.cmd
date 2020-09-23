d5947b7ec4812fbed0f16b7a5ef1e93b590a65d3
gwbrown <gwbrown@slac.stanford.edu>
2020-09-22 18:28:28 -0700

    Fixed iocAdmin prefix, SYS1 not SYS0.

--- iocBoot/sioc-sys1-sc01/st.cmd
+++ iocBoot/sioc-sys1-sc01/st.cmd
@@ -4,26 +4,26 @@
 
 < envPaths
 
-epicsEnvSet("IOC_NAME", "SIOC:SYS0:SC01")
+epicsEnvSet("IOC_NAME", "SIOC:SYS1:SC01")
 
 < ../common/generalInit.cmd
 pwd()
 
-epicsEnvSet("NODE_NAME", "scop-la20-ls21")
-epicsEnvSet("P", "SCOP:LA20:LS21")
-< iocBoot/common/tds.cmd
+#epicsEnvSet("NODE_NAME", "scop-la20-ls21")
+#epicsEnvSet("P", "SCOP:LA20:LS21")
+#< iocBoot/common/tds.cmd
 
-epicsEnvSet("NODE_NAME", "scop-li20-ex01")
-epicsEnvSet("P", "SCOP:LI20:EX01")
-< iocBoot/common/tds.cmd
+#epicsEnvSet("NODE_NAME", "scop-li20-ex01")
+#epicsEnvSet("P", "SCOP:LI20:EX01")
+#< iocBoot/common/tds.cmd
 
-epicsEnvSet("NODE_NAME", "scop-li20-ex02")
-epicsEnvSet("P", "SCOP:LI20:EX02")
-< iocBoot/common/tds.cmd
+#epicsEnvSet("NODE_NAME", "scop-li20-ex02")
+#epicsEnvSet("P", "SCOP:LI20:EX02")
+#< iocBoot/common/tds.cmd
 
-epicsEnvSet("NODE_NAME", "scop-li20-los1")
-epicsEnvSet("P", "SCOP:LI20:LOS1")
-< iocBoot/common/tds.cmd
+#epicsEnvSet("NODE_NAME", "scop-li20-los1")
+#epicsEnvSet("P", "SCOP:LI20:LOS1")
+#< iocBoot/common/tds.cmd
 
 epicsEnvSet("NODE_NAME", "scop-in10-fc01")
 epicsEnvSet("P", "SCOP:IN10:FC01")
