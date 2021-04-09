7da27d26358688b11c9dff4cb24ee8291a77c506
Kristi Luchini <luchini@slac.stanford.edu>
2021-04-09 09:26:16 -0700

    Add standard heaeder, chg db load, call common scripts, add caPutLogInit

--- iocBoot/sioc-lgun-sc01/st.cmd
+++ iocBoot/sioc-lgun-sc01/st.cmd
@@ -1,21 +1,55 @@
 #!../../bin/rhel6-x86_64/oscilloscope
+#==============================================================
+#
+#  Abs:  Startup Script for the Laser GunB Scope
+#
+#  Name: st.cmd
+#
+#  Desc:  This is the EPICS startup script for a soft IOC
+#         that will control the vacuum system in
+#         the FACET-II injector, which is located at
+#         sector 10. This IOC will be using the
+#         facet facility computer control infrastructure.
+#
+#  Facility:  LCLS Oscilliscope Controls
+#
+#  Auth: 29-Apr-2020, Garth Brown     (GBROWN):
+#  Rev:  dd-mmm-yyyy, Reviewer's Name (USERNAME)
+#--------------------------------------------------------------
+#  Mod:
+#        08-Apr-2020, K. Luchini      (LUCHINI):
+#         add standard header
+#         chg db loaded
+#
+#==============================================================
+#
+# Set environment variables
+epicsEnvSet("IOC_NAME"  , "SIOC:LGUN:SC01")
+epicsEnvSet("LOCATION"  ,"facet-daemon1")
+epicsEnvSet("ENGINEER"  ,"Garth Brown")
+epicsEnvSet("NODE_NAME" , "scop-lgun-ls02")
 
-#- You may have to change oscilloscope to something else
-#- everywhere it appears in this file
+# Load common startup script
+< ../common/st.cmd.soft
 
-< envPaths
+# Initalize hardware
+< iocBoot/common/init_asyn.cmd
 
-epicsEnvSet("IOC_NAME", "SIOC:LGUN:SC01")
-epicsEnvSet("NODE_NAME", "scop-lgun-ls02")
-epicsEnvSet("P", "SCOP:LGUN:LS02")
+# Load record instances
+dbLoadRecords("db/${IOC}.db")
 
-< ../common/generalInit.cmd
-pwd()
-< iocBoot/common/tds.cmd
+# Setup autosave/restore
+< iocBoot/common/autoSaveConf.cmd
 
-iocInit
+# Start EPICS
+iocInit()
 
-## Start any sequence programs
-#seq sncxxx,"user=gwbrown"
+# Turn on caPutLogging:
+# Log values only on change to the iocLogServer:
+caPutLogInit("${EPICS_CA_PUT_LOG_ADDR}")
+caPutLogShow(2)
 
+# Start autosave routines to save our data
 < iocBoot/common/autoSaveRun.cmd
+
+# End of file
