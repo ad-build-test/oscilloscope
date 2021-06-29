32ff90560fce4d968b52f54a308c6a4d20203548
Garth Brown <gwbrown@slac.stanford.edu>
2021-06-29 14:34:28 -0700

    Revert name to scop-mcc0-sc02.

--- iocBoot/sioc-sys1-sc01/st.cmd
+++ iocBoot/sioc-sys1-sc01/st.cmd
@@ -36,7 +36,7 @@ epicsEnvSet("ENGINEER" ,"Garth Brown")
 iocshLoad("$(TOP)/iocBoot/common/tds.cmd", "NODE_NAME=scop-li20-ex01,P=SCOP:LI20:EX01")
 iocshLoad("$(TOP)/iocBoot/common/tds.cmd", "NODE_NAME=scop-in10-fc01,P=SCOP:IN10:FC01")
 iocshLoad("$(TOP)/iocBoot/common/tds.cmd", "NODE_NAME=scop-in10-fc02,P=SCOP:IN10:FC02")
-iocshLoad("$(TOP)/iocBoot/common/tds.cmd", "NODE_NAME=scop-mcc0-fc02,P=SCOP:mcc0:fc02")
+iocshLoad("$(TOP)/iocBoot/common/tds.cmd", "NODE_NAME=scop-mcc0-sc02,P=SCOP:MCC0:SC02")
 
 # Setup autosave/restore
 < iocBoot/common/autoSaveConf.cmd
