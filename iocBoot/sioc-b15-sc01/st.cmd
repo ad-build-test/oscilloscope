0aa0e84a74e51841edbe4834a02b014ab205d941
Michael Dunning <mdunning@slac.stanford.edu>
2022-04-04 12:27:50 -0700

    Add dev IOCs for b15 and b34

---
+++ iocBoot/sioc-b15-sc01/st.cmd
@@ -0,0 +1,39 @@
+#!../../bin/rhel6-x86_64/oscilloscope
+#==============================================================
+#
+#  Abs:  Startup Script for the B15 DPO2024 scope
+#
+#  Name: st.cmd
+#
+#  Facility:  LCLS Oscilliscope Controls
+#
+#  Auth: 28-Apr-2022, M. Dunning (mdunning)
+#==============================================================
+#
+# Set environment variables
+epicsEnvSet("IOC_NAME"  ,"SIOC:B15:SC01")
+epicsEnvSet("LOCATION"  ,"lcls-dev1")
+epicsEnvSet("ENGINEER"  ,"Mike Dunning")
+
+# Load common startup script
+< ../common/st.cmd.soft
+
+# Start driver & load databases
+iocshLoad("$(TOP)/iocBoot/common/tds.cmd", "P=SCOP:B15:NW01,NODE_NAME=scop-amrf-rf03-lclsdev,REBOOT="#"")
+
+
+# Setup autosave/restore
+< $(TOP)/iocBoot/common/autoSaveConf.cmd
+
+# Start EPICS
+iocInit()
+
+# Turn on caPutLogging:
+# Log values only on change to the iocLogServer:
+#caPutLogInit("${EPICS_CA_PUT_LOG_ADDR}")
+#caPutLogShow(2)
+
+# Start autosave routines to save our data
+< $(TOP)/iocBoot/common/autoSaveRun.cmd
+
+# End of file
