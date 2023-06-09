7bda8cd3d87ec57802175475e03752ed28796a9d
Michael Dunning <mdunning@slac.stanford.edu>
2023-06-08 19:05:17 -0700

    Add MDO34 support

---
+++ iocBoot/common/mdo.cmd
@@ -0,0 +1,36 @@
+#==============================================================
+#
+#  Abs:  Common Tektronix MDO series startup script for soft IOCs
+#
+#  Name: mdo.cmd
+#
+#  Rem:  Upon entry we expect to be at location TOP
+#        and the following macros must be defined:
+#           P - scope control PV prefix, e.g. SCOP:LGUN:LS02
+#           NODE_NAME - Network host name of the scope, e.g. scop-lgun-ls02
+#
+#  Facility: EED
+#
+#  Auth: 08-Jun-2023, M. Dunning (mdunning)
+#==============================================================
+
+epicsEnvSet("UDPP", "${NODE_NAME}-vxi11")
+epicsEnvSet("PORT", "${NODE_NAME}-port")
+
+vxi11Configure("$(UDPP)","$(NODE_NAME)",0,0.0,"inst0",0,0)
+dbLoadRecords("db/asynRecord.db","P=$(P),R=ASYN_VXI11,PORT=$(UDPP),ADDR=0,IMAX=0,OMAX=0")
+
+drvMDOConfigure("$(PORT)","$(UDPP)")
+dbLoadRecords("db/asynRecord.db","P=$(P),R=ASYN_MDO,PORT=$(PORT),ADDR=0,IMAX=0,OMAX=0")
+
+#asynSetTraceMask("$(UDPP)", -1, 0x11)
+#asynSetTraceIOMask("$(UDPP)", -1, 0x1)
+
+#asynSetTraceMask("$(PORT)", -1, 0x11)
+#asynSetTraceIOMask("$(PORT)", -1, 0x1)
+
+dbLoadRecords("db/scope.db", "P=$(P),NAME=MDO,PORT=$(PORT)")
+dbLoadRecords("db/mdo.db", "P=$(P),PORT=$(PORT)")
+
+# End of script
+
---
+++ iocBoot/sioc-b34-sc02/st.cmd
@@ -0,0 +1,41 @@
+#!../../bin/rhel6-x86_64/oscilloscope
+#==============================================================
+#
+#  Abs:  Startup Script for the B15 DPO2024 scope
+#
+#  Name: st.cmd
+#
+#  Facility:  LCLS Oscilliscope Controls
+#
+#  Auth: 28-Apr-2022, M. Dunning (mdunning)
+#==============================================================
+
+# Set environment variables
+epicsEnvSet("IOC_NAME"  ,"SIOC:B34:SC02")
+epicsEnvSet("LOCATION"  ,"lcls-dev1")
+epicsEnvSet("ENGINEER"  ,"Mike Dunning")
+
+# Load common startup script
+< ../common/st.cmd.soft
+
+# Start driver & load databases
+iocshLoad("$(TOP)/iocBoot/common/mdo.cmd", "P=SCOP:B34:SC272,NODE_NAME=192.168.1.68")
+
+# Set up autosave/restore
+< $(TOP)/iocBoot/common/autoSaveConf.cmd
+
+# Start EPICS
+iocInit()
+
+# Turn on caPutLogging:
+# Log values only on change to the iocLogServer:
+#caPutLogInit("${EPICS_CA_PUT_LOG_ADDR}")
+#caPutLogShow(2)
+
+# Start autosave routines to save our data
+< $(TOP)/iocBoot/common/autoSaveRun.cmd
+
+cd("${TOP}")
+
+# End of file
+
