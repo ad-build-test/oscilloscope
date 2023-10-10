063474808d45fc13c1c5a5df42e7d3951a2c8b67
Michael Dunning <mdunning@slac.stanford.edu>
2023-10-10 13:44:12 -0700

    TESTFAC: Add dev IOC sioc-b128-sc01

---
+++ iocBoot/sioc-b128-sc01/st.cmd
@@ -0,0 +1,41 @@
+#!../../bin/rhel6-x86_64/oscilloscope
+#==============================================================
+#
+#  Abs:  Startup Script for the MDO34 scope
+#
+#  Name: st.cmd
+#
+#  Facility:  LCLS Oscilliscope Controls
+#
+#  Auth: 10-Oct-2023, M. Dunning (mdunning)
+#==============================================================
+
+# Set environment variables
+epicsEnvSet("IOC_NAME"  ,"SIOC:B128:SC01")
+epicsEnvSet("LOCATION"  ,"testfac-daemon1")
+epicsEnvSet("ENGINEER"  ,"Mike Dunning")
+
+# Load common startup script
+< ../common/st.cmd.soft
+
+# Start driver & load databases
+iocshLoad("$(TOP)/iocBoot/common/mdo.cmd", "P=SCOP:B128:SC01,NODE_NAME=scop-b128-sc01")
+
+# Set up autosave/restore
+< $(TOP)/iocBoot/common/autoSaveConf.cmd
+
+# Start EPICS
+iocInit()
+
+# Turn on caPutLogging:
+# Log values only on change to the iocLogServer:
+#caPutLogInit("${EPICS_CA_PUT_LOG_ADDR}")
+#caPutLogShow(2)
+
+# Start autosave routines to save our data
+< $(TOP)/iocBoot/common/autoSaveRun.cmd
+
+cd("${TOP}")
+
+# End of file
+
